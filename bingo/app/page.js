648f217099587e547466b274e2c86a1ef5ace94a
OHart-NAIT <ohart1@nait.ca>
2026-08-18 14:45:30 -0600

    Made placeholder code for the recently called and bingo card section

--- bingo/app/page.js
+++ bingo/app/page.js
@@ -2,6 +2,8 @@ import styles from "./page.module.css";
 
 // My components
 import Header from "@/components/header";
+import RecentlyCalled from "@/components/recently-called"
+import BingoCard from "@/components/bingo-card"
 
 export default function Home() {
   return (
@@ -10,8 +12,12 @@ export default function Home() {
       <Header />
       
       <main className={styles.main}>
-          
+          <RecentlyCalled/>
+
+          <BingoCard/>
       </main>
     </div>
   );
 }
+
+
---
+++ bingo/components/bingo-card.js
@@ -0,0 +1,10 @@
+import styles from "@/app/page.module.css";
+
+export default function BingoCard() {
+    return (
+        <div className={styles.bingoCard}>
+            <h2>Bingo Card</h2>
+        </div>
+        
+    )   
+}
