2cc48a20ccc7c9b1f26714a77159c4d362bfd970
OHart-NAIT <ohart1@nait.ca>
2026-08-18 12:43:42 -0600

    Created Header component and got rid of unnecessary starter code

---
+++ bingo/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    // This is a static component meaning it returns the same thing regardless of any data/props
+    return (
+    <h1> Bingo Time! </h1>
+    )
+    
+}
